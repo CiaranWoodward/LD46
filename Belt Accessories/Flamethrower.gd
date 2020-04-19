4ad57a7347db593e7d11a14a79e25de543f765bb
Ciaran Woodward <ciaranwoodward@gmail.com>
2020-04-19 22:02:52 +0100

    Enable shooting flamethrower

--- Belt Accessories/Flamethrower.gd
+++ Belt Accessories/Flamethrower.gd
@@ -1,16 +1,43 @@
 extends BeltItem
 
+onready var fireTimer = get_node("FireTimer")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var isfiring : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
+func _process(delta: float):
+	if isfiring:
+		for i in range(8):
+			if i == cur_rotsprite:
+				var pe = self.get_node("P" + str(i))
+				pe.rotate(pe.get_local_mouse_position().angle())
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_rotsprite(dir : int):
+	if dir == cur_rotsprite:
+		return
+	assert(dir >= 0 && dir <= 7)
+	for i in range(8):
+		if i == dir && isfiring:
+			self.get_node("P" + str(i)).preprocess = 1.9 - fireTimer.time_left
+			self.get_node("P" + str(i)).emitting = true
+		else:
+			self.get_node("P" + str(i)).emitting = false
+
+	.set_rotsprite(dir)
+
+func begin_fire():
+	isfiring = true
+	self.get_node("P" + str(cur_rotsprite)).emitting = true
+	fireTimer.start()
+
+func end_fire():
+	isfiring = false
+	self.get_node("P" + str(cur_rotsprite)).emitting = false
+	for i in range(8):
+		self.get_node("P" + str(i)).preprocess = 0.1
+
+func _shoot():
+	pass
