093eeffd9237b57ac16e6b1be3cd6627ced24053
Ciaran Woodward <ciaranwoodward@gmail.com>
2020-04-20 22:44:47 +0100

    Work on basket egg loss

--- Belt Accessories/Basket.gd
+++ Belt Accessories/Basket.gd
@@ -2,6 +2,7 @@ extends BeltItem
 
 onready var eggparts = get_node("EggParticles")
 onready var eggsplode = preload("res://Belt Accessories/Eggsplode.tscn")
+onready var area = get_node("CollisionShape2D")
 
 func _ready():
 	pass
@@ -11,6 +12,15 @@ func set_eggcontent(eggc : int):
 		var newsplode = eggsplode.instance()
 		add_child(newsplode)
 	
+#	if eggc > 0:
+#		area.disabled = false
+#	else:
+#		area.disabled = true
+	
 	eggparts.amount = eggc
 	eggparts.restart()
 	.set_eggcontent(eggc)
+
+func damage(damage : float, gpos : Vector2):
+	print("yo")
+	set_eggcontent(egg_content - 1)
